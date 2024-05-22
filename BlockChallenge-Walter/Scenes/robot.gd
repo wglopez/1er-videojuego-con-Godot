74c3b91dbe03854e75c6eda9092873b44c9ba849
Walter Gabriel Lopez <113866752+wglopez@users.noreply.github.com>
2024-05-21 22:29:28 -0300

    Add files via upload

---
+++ BlockChallenge-Walter/Scenes/robot.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 1100
+var has_double_jumped = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("idle")
+
+
+
+func _physics_process(delta):
+	
+	$AnimationPlayer.play("idle")
+	
+	if Input.is_action_pressed("ui_right"):
+		$Sprite2D.flip_h=false
+	
+	if Input.is_action_pressed("ui_left"):
+		$Sprite2D.flip_h=true
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		has_double_jumped = false
+	if Input.is_action_just_pressed("ui_up") and not is_on_floor() and not has_double_jumped:
+		has_double_jumped = true
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
